db042f25db1f867b9f4413de91b3fcd2c12b132b
Lee <leewhiz119@gmail.com>
2022-08-24 22:16:02 -0400

    imported deck.json

---
+++ deck.js
@@ -0,0 +1,106 @@
+[{
+    "Ace of Spades" :{
+    "value": "ace",
+    "suit": "spades"
+},
+    "Two of Spades" :{
+    "value": "two",
+    "suit": "spades"
+},
+    "Three of Spades" :{
+    "value": "three",
+    "suit": "spades"
+},
+    "Four of Spades" :{
+    "value": "four",
+    "suit": "spades"
+},
+    "Five of Spades" :{
+    "value": "five",
+    "suit": "spades"
+},
+    "Six of Spades" :{
+    "value": "six",
+    "suit": "spades"
+},
+    "Seven of Spades" :{
+    "value": "seven",
+    "suit": "spades"
+},
+    "Eight of Spades" :{
+    "value": "eight",
+    "suit": "spades"
+},
+    "Nine of Spades" :{
+    "value": "nine",
+    "suit": "spades"
+},
+    "Ten of Spades" :{
+    "value": "ten",
+    "suit": "spades"
+},
+    "Jack of Spades" :{
+    "value": "jack",
+    "suit": "spades"
+},
+    "Queen of Spades" :{
+    "value": "queen",
+    "suit": "spades"
+},
+    "King of Spades" :{
+    "value": "king",
+    "suit": "spades"
+},
+    "Ace of Clubs" :{
+    "value": "ace",
+    "suit": "clubs"
+},
+    "Two of Clubs" :{
+    "value": "two",
+    "suit": "clubs"
+},
+    "Three of Spades" :{
+    "value": "three",
+    "suit": "clubs"
+},
+    "Four of Spades" :{
+    "value": "four",
+    "suit": "clubs"
+},
+    "Five of Spades" :{
+    "value": "five",
+    "suit": "clubs"
+},
+    "Six of Spades" :{
+    "value": "six",
+    "suit": "clubs"
+},
+    "Seven of Spades" :{
+    "value": "seven",
+    "suit": "clubs"
+},
+    "Eight of Spades" :{
+    "value": "eight",
+    "suit": "clubs"
+},
+    "Nine of Spades" :{
+    "value": "nine",
+    "suit": "clubs"
+},
+    "Ten of Spades" :{
+    "value": "ten",
+    "suit": "clubs"
+},
+    "Jack of Spades" :{
+    "value": "jack",
+    "suit": "clubs"
+},
+    "Queen of Spades" :{
+    "value": "queen",
+    "suit": "clubs"
+},
+    "King of Spades" :{
+    "value": "king",
+    "suit": "clubs"
+}
+}]
--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 let card = document.querySelectorAll('.card')
+fetch("./deck.json")
+.then(response=>{
+    return response.json()
+})
+.then(data=>{console.log(data)})
+
+let createCards=(suit, i)=> {
+    return `${i} of ${suit}`
+}
+
+let suits=["clubs", "spades", "hearts", "diamonds"]
+let numbers=["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
+
+let numberCycle=(numbers)=>{
+    for(let i=0; i<10; i++) {
+        return numbers[i]
+}
+}
+
+let deck = (suits) => {
+    suits.forEach((suit)=>{
+        for(let i=0; i<10; i++) {
+            card=suit[i]
+            return card
+        } 
+        }   
+    )
+}
+
+console.log(deck)
+
