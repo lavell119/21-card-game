fca0afcad1012b1955d21663ff1a0744e95b3f98
Lee <leewhiz119@gmail.com>
2022-08-24 23:18:14 -0400

    seperated deck.json elements

--- script.js
+++ script.js
@@ -1,24 +1,17 @@
 let deck
 let card = document.querySelectorAll('.card')
+let suits=["clubs", "spades", "hearts", "diamonds"]
+let numbers=["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
+
 
 fetch("./deck.json")
 .then(response=>{
     return response.json()
 })
 .then(data=>deck=data)
-.then(deck=>console.log(deck))
-let createCards=(suit, i)=> {
-    return `${i} of ${suit}`
-}
+.then(deck=>console.log(deck))   
 
-let suits=["clubs", "spades", "hearts", "diamonds"]
-let numbers=["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
 
-let numberCycle=(numbers)=>{
-    for(let i=0; i<10; i++) {
-        return numbers[i]
-}
-}
 
 
 
