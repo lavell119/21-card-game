4b1b0b6ceb91b7030be9b7deb5cee867c56bda83
Lee <leewhiz119@gmail.com>
2022-08-25 20:45:31 -0400

    added random card functionality and displaying card data inside card element

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ let card = document.querySelectorAll('.card')
 let suits=["clubs", "spades", "hearts", "diamonds"]
 let numbers=["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
 let startButton=document.querySelector(".start-button")
+let topButtons=document.querySelectorAll(".top-button")
+let hitButton=document.querySelector(".hit-button")
+let stayButton=document.querySelector(".stay-button")
 
 
 fetch("./deck.json")
@@ -12,8 +15,19 @@ fetch("./deck.json")
 .then(data=>deck=data)
 .then(deck=>console.log(deck))   
 
+startButton.addEventListener('click', ()=>{
+    hitButton.classList.remove("hide")
+    stayButton.classList.remove("hide")
+    startButton.classList.add('hide')
+})
 
+hitButton.addEventListener('click', ()=>{
+    let i = Math.floor(Math.random() * 52)
+    let drawnCard=(deck[i])
 
+    console.log(Object.entries(drawnCard))
+    card[0].innerText=Object.entries(drawnCard)
+})
 
 
 
